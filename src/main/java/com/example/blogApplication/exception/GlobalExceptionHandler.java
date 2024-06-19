eb991434be1f935ef3244b994180fb8967b26e25
Devraj Mudgal <devrajmudgal17@gmail.com>
2024-06-20 01:43:37 +0530

    Added Validation CommentDto class

--- src/main/java/com/example/blogApplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/blogApplication/exception/GlobalExceptionHandler.java
@@ -18,39 +18,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException, WebRequest webRequest){
-        ErrorDetails errorDetails = new ErrorDetails(new Date(),resourceNotFoundException.getMessage(),webRequest.getDescription(false));
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), resourceNotFoundException.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BlogAPIException.class)
-    public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException blogAPIException, WebRequest webRequest){
-        ErrorDetails errorDetails = new ErrorDetails(new Date(),blogAPIException.getMessage(),webRequest.getDescription(false));
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException blogAPIException, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), blogAPIException.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest){
-        ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(),webRequest.getDescription(false));
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        Map<String,String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error)->{
-            String fieldName = ((FieldError)error).getField();
-            String message = error.getDefaultMessage();
-            errors.put(fieldName,message);
-        });
-        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
-    }
-
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest webRequest){
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 //        Map<String,String> errors = new HashMap<>();
 //        ex.getBindingResult().getAllErrors().forEach((error)->{
 //            String fieldName = ((FieldError)error).getField();
@@ -59,5 +49,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 //        });
 //        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
 //    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest webRequest) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
+
 
--- src/main/java/com/example/blogApplication/payload/CommentDto.java
+++ src/main/java/com/example/blogApplication/payload/CommentDto.java
@@ -1,11 +1,20 @@
 package com.example.blogApplication.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private Long id;
+
+    @NotEmpty(message = "Name must not be Null or Empty")
     private String name;
+    @NotEmpty(message = "Email must not be Null or Empty")
+    @Email
     private String email;
+    @NotEmpty(message = "Comment Body must not be Null or Empty")
+    @Size(min = 10,message = "Comment Body Must Be at least 10 Characters long")
     private String body;
 }
